afb86d49f92478b7018f0f85631c6dd248b12eac
Agent Sivji <alysivji+agent@gmail.com>
2026-04-07 15:34:23 -0500

    use config in agent

--- agent/main.py
+++ agent/main.py
@@ -1,22 +1,15 @@
-import os
-
 import dotenv
 from strands import Agent
 from strands.models.openai import OpenAIModel
 
+from .config import OPENAI_API_KEY, OPENAI_MODEL
 from .tools import list_ansible_playbooks, run_ansible_playbook
 
 dotenv.load_dotenv()
 
-OPENAI_KEY = os.getenv("OPENAI_API_KEY")
-if not OPENAI_KEY:
-    raise ValueError("OPENAI_API_KEY environment variable is not set.")
-
-OPENAI_MODEL = os.getenv("OPENAI_MODEL", "gpt-4o-mini")
-
 model = OpenAIModel(
     client_args={
-        "api_key": os.getenv("OPENAI_API_KEY"),
+        "api_key": OPENAI_API_KEY,
     },
     model_id=OPENAI_MODEL,
 )
